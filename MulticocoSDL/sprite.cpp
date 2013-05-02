cf2a23e4be24a4debd8d7bb9fe9c627a9d754e49
José Ladislao Lainez Ortega <thedragonladis@gmail.com>
2013-05-02 13:21:09 +0200

    Movidos los archivos de la clase Window
    Movidos al lugar correcto dentro de la carpeta MulticocoSDL.

---
+++ MulticocoSDL/sprite.cpp
@@ -0,0 +1,9 @@
+//
+//  sprite.cpp
+//  MulticocoSDL
+//
+//  Created by Jose Ladislao Lainez Ortega on 5/2/13.
+//  Copyright (c) 2013 José Ladislao y José Molina. All rights reserved.
+//
+
+#include "sprite.h"
---
+++ MulticocoSDL/sprite.h
@@ -0,0 +1,14 @@
+//
+//  sprite.h
+//  MulticocoSDL
+//
+//  Created by Jose Ladislao Lainez Ortega on 5/2/13.
+//  Copyright (c) 2013 José Ladislao y José Molina. All rights reserved.
+//
+
+#ifndef __MulticocoSDL__sprite__
+#define __MulticocoSDL__sprite__
+
+#include <iostream>
+
+#endif /* defined(__MulticocoSDL__sprite__) */
